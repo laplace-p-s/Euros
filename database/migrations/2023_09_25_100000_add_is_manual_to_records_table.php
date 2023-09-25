7c94a6c738e881aaeb49b5e4aad3b916c0e66060
laplace_p_s <laplace2012sao@gmail.com>
2023-09-25 17:06:23 +0900

    Migration file addition of database column 'is_manual' for manual additional record identification
    php artisan make:migration add_is_manual_to_records_table --table=records

---
+++ database/migrations/2023_09_25_100000_add_is_manual_to_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    //php artisan make:migration add_is_manual_to_records_table --table=records
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->integer('is_manual')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->dropColumn('is_manual');
+        });
+    }
+};
