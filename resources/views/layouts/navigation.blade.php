ee0713c0b15f380c911b08b175f44bfad31da20f
laplace_p_s <laplace2012sao@gmail.com>
2026-08-05 12:24:01 +0900

    Searchタブのハイライトが月変更・詳細表示時に消える不具合を修正
    POST(search.post)および詳細(detail)ルートでもSearchナビリンクの
    active判定が有効になるよう条件を追加。デスクトップ・モバイル両対応。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('search')" :active="request()->routeIs('search')">
+                    <x-nav-link :href="route('search')" :active="request()->routeIs('search') || request()->routeIs('search.post') || request()->routeIs('detail')">
                         <i class="fas fa-calendar-day"></i>&nbsp;{{ __('Search') }}
                     </x-nav-link>
                 </div>
@@ -87,7 +87,7 @@
             </x-responsive-nav-link>
         </div>
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('search')" :active="request()->routeIs('search')">
+            <x-responsive-nav-link :href="route('search')" :active="request()->routeIs('search') || request()->routeIs('search.post') || request()->routeIs('detail')">
                 <i class="fas fa-calendar-day"></i>&nbsp;{{ __('Search') }}
             </x-responsive-nav-link>
         </div>
